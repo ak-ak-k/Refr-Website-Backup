69e2f2250fdfc6fd92e3f63a80cf85b9b608015e
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-08-31 09:35:07 +0530

    update

---
+++ fees_paymentpolicy_merchant/assets/js/testimonial.js
@@ -0,0 +1,34 @@
+// testimoninals
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// swiper
+var swiper = new Swiper(".mySwiper1", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: "3",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
+});
