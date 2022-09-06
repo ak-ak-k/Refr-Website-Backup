229133fcac4cadfece02d0acb111776d696b834b
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-09-06 17:41:42 +0530

    update

--- blogs/Refr-Blog-Main/public/script.js
+++
@@ -1,9 +0,0 @@
-// function myFunction(y) {
-//   var x = document.getElementById("myLinks");
-//   y.classList.toggle("change");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
