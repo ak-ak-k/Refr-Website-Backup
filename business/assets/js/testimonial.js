6eb746d0f618bcccd4b1584384dafddebf9462b0
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-12-08 15:41:46 +0530

    testimonial update

--- business/assets/js/testimonial.js
+++ business/assets/js/testimonial.js
@@ -1,53 +1,14 @@
-var catarr = [];
-// testimoninals
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  hashNavigation: {
-    watchState: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-changeswiper();
+var x = window.matchMedia("(min-width: 700px)");
+swiper(x);
+x.addListener(swiper);
 
-// swiper
-var swiper = new Swiper(".mySwiper1", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: false,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination1",
-    clickable: true,
-  },
-});
-
-addEventListener("resize", (event) => {
-  width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-  changeswiper();
-});
-
-function changeswiper() {
-  if (width <= 700) {
-    new Swiper(".mySwiper1", {
+function swiper() {
+  if (x.matches) {
+    var swiper = new Swiper(".mySwiper1", {
       effect: "coverflow",
       grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "1",
+      centeredSlides: false,
+      slidesPerView: 3,
       coverflowEffect: {
         rotate: 50,
         stretch: 0,
@@ -61,11 +22,11 @@ function changeswiper() {
       },
     });
   } else {
-    new Swiper(".mySwiper1", {
+    var swiper = new Swiper(".mySwiper1", {
       effect: "coverflow",
       grabCursor: true,
       centeredSlides: false,
-      slidesPerView: "3",
+      slidesPerView: "auto",
       coverflowEffect: {
         rotate: 50,
         stretch: 0,
